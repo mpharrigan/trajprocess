881c902b199fbc8a5892366349bcfa9e620c589e
Matthew Harrigan <harrigan@stanford.edu>
2015-09-21 15:51:34 -0700

    Add test for cnv

--- trajprocess/tests/test_cnv.py
+++ trajprocess/tests/test_cnv.py
@@ -1,11 +1,9 @@
-import re
-import subprocess
 import os
 
 from nose import with_setup
 import mdtraj
 
-from .utils import generate_project, cleanup, write_run_clone
+from .utils import generate_project, cleanup
 from trajprocess.files import Project, record
 
 
@@ -18,14 +16,11 @@ def test_cnv():
     cnv_infos = list(map(record(project.cnv), cat_infos))
 
     assert len(cnv_infos) > 0
-    return True
 
-    for info in cat_infos:
-        assert info['cat']['gen'] == 2  # per construction
-        assert info['cat']['success']
-        assert os.path.exists(info['cat']['xtc_out'])
+    for info in cnv_infos:
+        assert os.path.exists(info['cnv']['xtc_out'])
 
-        with mdtraj.open(info['cat']['xtc_out']) as tfile:
+        with mdtraj.open(info['cnv']['xtc_out']) as tfile:
             xyz, time, step, box = tfile.read()
             print("Shape", xyz.shape)
-            assert xyz.shape == (20, 7, 3)
+            assert xyz.shape == (21, 22, 3), xyz.shape
