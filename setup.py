5f868886feae8c587ac7337bfb0e2c0c54a5ba46
Matthew Harrigan <harrigan@stanford.edu>
2015-10-09 15:35:51 -0700

    Install testing data

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="trajprocess",
-    version='0.28',
+    version='0.29',
     packages=find_packages(),
     requires=['numpy', 'mdtraj', 'nose'],
     zip_safe=False,
-    package_data={
-        'trajprocess.tests': ['topol.tpr']
-    },
+    include_package_data=True,
 )
