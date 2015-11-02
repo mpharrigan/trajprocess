d611830525e93e1c1a364ed88695d62003490e07
Matthew Harrigan <harrigan@stanford.edu>
2015-11-02 10:20:38 -0800

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="trajprocess",
-    version='2.0.4',
+    version='2.0.5',
     packages=find_packages(),
     requires=['numpy', 'mdtraj', 'nose'],
     zip_safe=False,
