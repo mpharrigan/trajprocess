0802cdbc19576ddd6c074e1bb1dcd1853094b43e
Matthew Harrigan <harrigan@stanford.edu>
2015-11-12 14:56:59 -0800

    update get reference data

--- trajprocess/tests/get_addtl_ref_data.sh
+++ trajprocess/tests/get_addtl_ref_data.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-wget http://web.stanford.edu/~harrigan/mock3-reference.tar.bz2
-tar -xf mock3-reference.tar.bz2
 
-wget http://web.stanford.edu/~harrigan/mock4-reference.tar.bz2
-tar -xf mock4-reference.tar.bz2
+if [ ! -e mock3-reference.tar.bz2 ]
+then
+    wget http://web.stanford.edu/~harrigan/mock3-reference.tar.bz2
+    tar -xf mock3-reference.tar.bz2
+fi
+
+if [ ! -e mock4-reference.tar.bz2 ]
+then
+    wget http://web.stanford.edu/~harrigan/mock4-reference.tar.bz2
+    tar -xf mock4-reference.tar.bz2
+fi
