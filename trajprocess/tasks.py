c21ce42be55282bce81af1596566cd898af3248a
Matthew Harrigan <harrigan@stanford.edu>
2016-01-07 11:43:41 -0800

    flesh out dependency structure

--- trajprocess/tasks.py
+++ trajprocess/tasks.py
@@ -23,41 +23,90 @@ class PRCTask(Task):
         self.gen = gen
 
     @property
-    def is_done(self):
-        return os.path.exists("{outdir}/{prc:dir}/{code}/{gen}.{fext}"
-                              .format(outdir=config.outdir,
-                                      prc=self.prc,
-                                      code=self.code,
-                                      gen=self.gen,
-                                      fext=self.fext))
+    def fn(self):
+        return ("{outdir}/{prc:dir}/{code}/{gen}.{fext}"
+                .format(outdir=config.outdir,
+                        code=self.code,
+                        prc=self.prc,
+                        gen=self.gen,
+                        fext=self.fext)
+                )
 
-    def do(self, prior):
-        out_fn = ("{outdir}/{prc:dir}/{code}/{gen}.nc"
-                  .format(outdir=config.outdir,
-                          code=self.code,
-                          prc=self.prc,
-                          gen=self.gen)
-                  )
+    @property
+    def is_done(self):
+        return os.path.exists(self.fn)
+
+    def do(self, task):
+        in_fn = task.fn
+        if not os.path.exists(in_fn):
+            raise FileNotFoundError("Input file for task {} not found. "
+                                    "Looking for {}"
+                                    .format(self, in_fn))
+
+        out_fn = self.fn
         out_dir = os.path.dirname(out_fn)
         os.makedirs(out_dir, exist_ok=True)
-        self.do_file(prior, out_fn)
+        self.do_file(in_fn, out_fn)
+
+    def __str__(self):
+        return "<{} {} {}>".format(self.prc, self.gen, self.code)
 
     def do_file(self, infn, outfn):
         raise NotImplementedError
 
 
-class Strip(Task):
-    def __init__(self, prc, gen):
-        self.prc = prc
-        self.gen = gen
-
-    @property
-    def depends(self):
-        yield from []
-
+class RawXTC(Task):
     @property
     def is_done(self):
         return True
+
+    @property
+    def fn(self):
+        return "{indir}/RUN{run}/CLONE{clone}/frame{gen}.xtc"
+
+    def do(self, tasks):
+        return
+
+
+class Trjconv(PRCTask):
+    code = 'cnv1'
+    fext = 'xtc'
+
+    @property
+    def depends(self):
+        yield RawXTC()
+
+    def do_file(self, infn, outfn):
+        assert os.path.exists(infn)
+        subprocess.call(['touch', outfn])
+
+
+class ConvertToNC(PRCTask):
+    code = 'cnv2'
+
+    @property
+    def depends(self):
+        self.is_gromacs = True
+        if self.is_gromacs:
+            yield Trjconv(self.prc, self.gen)
+        else:
+            yield RawXTC()
+
+    def do_file(self, infn, outfn):
+        assert os.path.exists(infn)
+        subprocess.call(['touch', outfn])
+
+
+class Strip(PRCTask):
+    code = 'stp'
+
+    @property
+    def depends(self):
+        yield ConvertToNC(self.prc, self.gen)
+
+    def do_file(self, infn, outfn):
+        assert os.path.exists(infn)
+        subprocess.call(['touch', outfn])
 
 
 class Center(PRCTask):
@@ -65,10 +114,11 @@ class Center(PRCTask):
 
     @property
     def depends(self):
-        yield from [Strip(self.prc, self.gen)]
+        yield Strip(self.prc, self.gen)
 
     def do_file(self, infn, outfn):
-        subprocess.call(['touch', 'outfn'])
+        assert os.path.exists(infn)
+        subprocess.call(['touch', outfn])
 
 
 class Project(Task):
