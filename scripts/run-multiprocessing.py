d72729d4c4e86dfbf600ea5769eb185b143ffaf7
Matthew Harrigan <harrigan@stanford.edu>
2016-01-19 16:40:31 -0800

    don't do no_cpptraj

--- scripts/run-multiprocessing.py
+++ scripts/run-multiprocessing.py
@@ -1,6 +1,6 @@
 from trajprocess.multiprocessing import Pool
 from trajprocess.app import execute_task
-from trajprocess.systems.nav_no_cpptraj import NaV
+from trajprocess.systems.nav import NaV
 
 with Pool(14) as lbv:
     execute_task(NaV(), lbv)
