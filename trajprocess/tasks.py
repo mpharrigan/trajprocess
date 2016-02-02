5aec62ee2348505938b3b57579db1749916c8f38
Matthew Harrigan <harrigan@stanford.edu>
2016-02-02 11:10:51 -0800

    bugfix

--- trajprocess/tasks.py
+++ trajprocess/tasks.py
@@ -156,10 +156,10 @@ class PRCGTask(Task):
 
     @property
     def log_fn(self):
-        return ("{outdir}/{prcg:dir}/{code}/{prc:gen}.{fext}"
+        return ("{outdir}/{prcg:dir}/{code}/{prcg:gen}.{fext}"
                 .format(outdir=config.outdir,
                         code=self.code,
-                        prc=self.prcg,
+                        prcg=self.prcg,
                         fext='log')
                 )
 
