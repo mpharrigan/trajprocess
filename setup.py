104011396198cd36fcd92c3d0fc6e4d78fb02948
Matthew Harrigan <harrigan@stanford.edu>
2016-02-02 13:10:13 -0800

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name="trajprocess",
-        version='4.0.12',
+        version='4.0.13',
         packages=find_packages(),
         zip_safe=False,
         include_package_data=True,
